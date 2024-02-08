949d19e844cd871190125ad0b6f5a3c24d86c3c3
robbdouglas <robert.nimmrich@icloud.com>
2024-02-08 09:43:17 +0100

    add placeholders, margin and basic routing

--- src/components/NewGameForm.jsx
+++ src/components/NewGameForm.jsx
@@ -8,14 +8,14 @@ function NewGameForm() {
     <div>
       <h1>New Game Form</h1>
         <form>
-            <label htmlFor="player-name">Player Name</label>
-            <input type="text" id="player-name" name="player-name" />
-            <label htmlFor="hero">Hero</label>
-            <input type="text" id="hero" name="hero" />
-            <label htmlFor="placement">Placement</label>
-            <input type="number" id="placement" name="placement" />
-            <label htmlFor="tribe">Tribe</label>
-            <input type="text" id="tribe" name="tribe" />
+            <label htmlFor="player-name">Player Name:</label>
+            <input type="text" id="player-name" name="player-name" placeholder="Enter your name..."/>
+            <label htmlFor="hero">Hero:</label>
+            <input type="text" id="hero" name="hero" placeholder="Enter your hero..."/>
+            <label htmlFor="placement">Placement:</label>
+            <input type="number" id="placement" name="placement" placeholder="Enter your placement..."/>
+            <label htmlFor="tribe">Tribe:</label>
+            <input type="text" id="tribe" name="tribe" placeholder="Enter your tribe..."/>
             <button type="submit">Submit</button>
         </form>
         <Link to="/">
