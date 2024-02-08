eced561afb864fd42368735eee1fe4d8cf7333cb
robbdouglas <robert.nimmrich@icloud.com>
2024-02-08 10:56:15 +0100

    remove AppContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 // App.jsx
+
 import React from "react";
 import "./styles/App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
-import NewGameForm from "./components/NewGameForm";
-import Stats from "./components/Stats";
-import { AppProvider } from "./contexts/AppContext";
+import NewGameForm from "./components/NewGameForm"; // Annahme: Die Komponente NewGameForm existiert bereits
+import Stats from "./components/Stats"; // Annahme: Die Komponente Stats existiert bereits
 
 function App() {
   return (
-    <AppProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/new-game-form" element={<NewGameForm />} />
-          <Route path="/stats" element={<Stats />} />
-          <Route path="*" element={<h1>Error 404: Page not found</h1>} />
-        </Routes>
-      </Router>
-    </AppProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/new-game-form" element={<NewGameForm />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="*" element={<h1>Error 404: Page not found</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
